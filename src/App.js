619c8f682cc025b8e118c2cb509ab848abab1773
Patrick <p@rickjobin.com>
2020-02-08 16:43:42 -0500

    added collection previews

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
-import Homepage from './pages/homepage/homepage.component'
+import HomePage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component'
+import {Route, Switch, BrowserRouter} from 'react-router-dom'
 
 function App() {
   return (
     <div>
-      <Homepage></Homepage>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop/' component={ShopPage} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
